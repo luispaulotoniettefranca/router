1864226ea4b1bf5d76d3d66996a216ca79ad80b7
luispaulotoniettefranca <luis_ti@outlook.com>
2021-02-15 11:08:38 -0300

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -35,7 +35,7 @@ class Request
                 foreach ($_SERVER as $name => $value) {
                     if (substr($name, 0, 5) == 'HTTP_') {
                         $headers[str_replace(' ', '-', 
-                                 ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                        ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
                     }
                 }
                 return $headers;
